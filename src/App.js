003b96cbbc66e9f0307cfbb880f8682f2e4d8405
PerisOduol618 <oduolpepe618@gmail.com>
2022-03-24 00:27:18 +0300

    Add simple component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React,{Component} from "react";
+import Table from './Table'
 
-class App extends React.Component{
+class App extends                                                                                                                                                                                               Component{
     render() {
       return(
         
-        <div className= "App">
-          <h1>Hello,World!</h1>
+        <div className= "container">
+          <Table />
         </div>
       )
     }
   }
-
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
   export default App
